bf807e1ab8276f226b0c9bd781623cb18cb361af
LauraKLA <laura1192@hotmail.com>
2024-03-16 22:41:52 -0500

    Ejercicio 12

---
+++ Ejercicio12/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int contador = 0;
+        int minimo = int.MaxValue;
+
+        Console.WriteLine("Ingresa cinco números:");
+
+        while (contador < 5)
+        {
+            Console.Write($"Número {contador + 1}: ");
+            string input = Console.ReadLine();
+            int numero;
+
+            if (int.TryParse(input, out numero))
+            {
+                if (numero < minimo)
+                {
+                    minimo = numero;
+                }
+
+                contador++;
+            }
+            else
+            {
+                Console.WriteLine("¡ERROR! este campo debe ser numérico. Intenta nuevamente!");
+            }
+        }
+
+        Console.WriteLine($"El número más pequeño es: {minimo}");
+    }
+}
