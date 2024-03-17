ee8803a4e22f2bcf4ce0c1e9deaad713a69d7f52
LauraKLA <laura1192@hotmail.com>
2024-03-16 23:09:39 -0500

    Ejercicio 15

---
+++ Ejercicio15/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = 0;
+        bool esNumeroValido = false;
+
+        while (!esNumeroValido)
+        {
+            Console.Write("ingrese un número: ");
+            string input = Console.ReadLine();
+
+            esNumeroValido = int.TryParse(input, out numero);
+
+            if (!esNumeroValido)
+            {
+                Console.WriteLine("¡ERROR! este campo permite sólo valores numéricos.");
+                continue; 
+            }
+            if (numero >= 10 && numero <= 20)
+            {
+                Console.WriteLine($"¡ES CORRECTO! el número {numero} está entre 10 y 20. ");
+            }
+            else
+            {
+                Console.WriteLine($"¡ERROR! el número {numero} no está entre 10 y 20. Intenta de nuevo!");
+                esNumeroValido = false; 
+            }
+        }
+    }
+}
