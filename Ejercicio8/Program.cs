7a01eb829b7146113338b3bf84812473ca8542b5
LauraKLA <laura1192@hotmail.com>
2024-03-16 22:00:49 -0500

    Ejercicio 8

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        int suma = 0;
+
+        for (int i = 1; i <= 50; i++)
+        {
+
+            if (i % 2 == 0)
+            {
+
+                suma += i;
+            }
+        }
+
+        Console.WriteLine("La suma de los números pares entre 1 y 50 es: " + suma);
+
+    }
+}
