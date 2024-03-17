09bfe3e2d5457603233ebd12ed99a9ad753d5a43
LauraKLA <laura1192@hotmail.com>
2024-03-16 22:25:01 -0500

    Ejercicio 10

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string palabra;
+        int longitud;
+
+        do
+        {
+            Console.Write("Ingresa una palabra: ");
+            palabra = Console.ReadLine().Trim();
+
+            if (int.TryParse(palabra, out _))
+            {
+                Console.WriteLine("Este campo no es númerico, por favor ingresa una palabra.");
+                continue;
+            }
+            else
+            {
+                longitud = palabra.Length;
+                Console.WriteLine($"La longitud de la palabra '{palabra}' es: {longitud}");
+                break;
+            }
+        } while (true);
+    }
+}
