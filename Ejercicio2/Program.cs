8e310bc6f2297bcf37b8e065c4b7d7298eb76374
LauraKLA <laura1192@hotmail.com>
2024-03-16 20:16:23 -0500

    Ejercicio 2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        int resultado;
+
+        Console.WriteLine("Ingrese el primer número:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+        if (num1 > num2)
+        {
+            resultado = num1 * 2;
+        }
+        else
+        {
+            resultado = num2 * 3;
+        }
+
+        Console.WriteLine("El resultado es: " + resultado);
+
+    }
+}
