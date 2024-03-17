8bf47cadc8fad194194e8e0b76fedd4b1f647d94
LauraKLA <laura1192@hotmail.com>
2024-03-16 20:02:45 -0500

    Ejercicio 1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double num = 0;
+        double resultado = 0;
+
+        while (true) 
+        {
+            Console.WriteLine("Ingresa un número:");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out num))
+            {
+                if (num > 0)
+                {
+                    resultado = (num * num);
+                    Console.WriteLine($"El cuadrado de {num} es: {resultado}");
+                    break; 
+                }
+                else
+                {
+                    Console.WriteLine("¡ERROR! El número ingresado debe ser positivo.");
+                }
+            }
+            
+        }
+    }
+
+}
