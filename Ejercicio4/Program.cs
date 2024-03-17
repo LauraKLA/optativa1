19405c61af1edfb70f18853a8f6599f66d22eac7
LauraKLA <laura1192@hotmail.com>
2024-03-16 20:40:17 -0500

    Ejercicio 4

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double radio;
+        double perimetro;
+        bool num;
+
+        do
+        {
+            Console.Write("Indique el radio del círculo: ");
+            string input = Console.ReadLine();
+            
+            
+            num = double.TryParse(input, out radio);
+
+            if (!num || radio <= 0)
+            {
+                Console.WriteLine("¡ERROR! Introduzca un valor numérico ");
+                num = false; 
+            }
+
+        } while (!num);
+
+    
+        perimetro = 2 * Math.PI * radio;
+
+        Console.WriteLine($"Para un círculo con radio {radio} el perímetro es: {perimetro}");
+    }
+}
