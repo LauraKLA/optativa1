b948d76d702b9f89a2f99f488c47b08f134fcfe3
LauraKLA <laura1192@hotmail.com>
2024-03-16 21:38:39 -0500

    Ejercicio 6

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double salarioAnual = 0;
+        bool esValido = false;
+
+
+        while (!esValido)
+        {
+            Console.Write("Ingrese su salario anual: ");
+            string input = Console.ReadLine();
+
+
+            if (double.TryParse(input, out salarioAnual))
+            {
+
+                if (salarioAnual >= 0)
+                {
+                    esValido = true;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("¡ERROR! el valor ingresado debe ser numérico.");
+            }
+        }
+
+
+        if (salarioAnual > 12000)
+        {
+            double excedente = salarioAnual - 12000;
+            double impuesto = excedente * 0.15;
+            Console.WriteLine("El impuesto a pagar es: $" + " " + impuesto);
+        }
+        else
+        {
+            Console.WriteLine("No debe que pagar impuestos.");
+        }
+    }
+}
